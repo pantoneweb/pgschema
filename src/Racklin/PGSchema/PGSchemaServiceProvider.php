659fd0a3e9953896400fc36c8a42115b88b71c15
Rafael Silva <djras_rafael@hotmail.com>
2021-03-25 17:18:58 -0300

    Corrigindo parametro necessário no construtor

--- src/Racklin/PGSchema/PGSchemaServiceProvider.php
+++ src/Racklin/PGSchema/PGSchemaServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Racklin\PGSchema;
 
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Support\ServiceProvider;
 use Racklin\PGSchema\Commands\PGMigrateCommand;
 use Racklin\PGSchema\Commands\PGRefreshCommand;
@@ -28,7 +29,7 @@ class PGSchemaServiceProvider extends ServiceProvider
             return new PGSchema();
         });
         $this->app->singleton('pgschema.migrate', function ($app) {
-            return new PGMigrateCommand($app['migrator']);
+            return new PGMigrateCommand($app['migrator'], $app[Dispatcher::class]);
         });
         $this->app->singleton('pgschema.rollback', function ($app) {
             return new PGRollbackCommand($app['migrator']);
