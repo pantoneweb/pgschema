dbec98b664a7bdffea902f972296bd7dd8bec060
Rafael Silva <djras_rafael@hotmail.com>
2021-03-26 11:43:45 -0300

    Atualizando campos novos do laravel 8

--- src/Racklin/PGSchema/Commands/PGMigrateCommand.php
+++ src/Racklin/PGSchema/Commands/PGMigrateCommand.php
@@ -15,13 +15,13 @@ class PGMigrateCommand extends MigrateCommand
      */
     protected $signature = 'pgschema:migrate {--database= : The database connection to use.}
                 {--schema= : The database schema to use.}
-                {--force : Force the operation to run when in production.}
-                {--path= : The path of migrations files to be executed.}
-                {--pretend : Dump the SQL queries that would be run.}
-                {--seed : Indicates if the seed task should be re-run.}
-                {--step : Force the migrations to be run so they can be rolled back individually.}';
-
-
+                {--force : Force the operation to run when in production}
+                {--path=* : The path(s) to the migrations files to be executed}
+                {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
+                {--schema-path= : The path to a schema dump file}
+                {--pretend : Dump the SQL queries that would be run}
+                {--seed : Indicates if the seed task should be re-run}
+                {--step : Force the migrations to be run so they can be rolled back individually}';
     /**
      * Execute the console command.
      *
